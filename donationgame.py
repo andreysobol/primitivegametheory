9903251972299bff160645850f5d6a3c91c27f89
Andrey Sobol <baskerville@andreysobol.com>
2020-06-01 20:22:43 +0300

    calculate_payoffs

--- donationgame.py
+++ donationgame.py
@@ -1,10 +1,24 @@
 players = ("player1", "player2")
 
-strategies = ("cheat", "cooperate")
+strategies = ("cooperate", "cheat")
 
 player1_strategies = strategies
 player2_strategies = strategies
 
 from itertools import product
 
-multiplied_strategies = product(player1_strategies, player2_strategies)
+multiplied_strategies = list(product(player1_strategies, player2_strategies))
+
+def calculate_payoffs(multiplied_strategies):
+    r = {}
+    for strategies in multiplied_strategies:
+        if strategies == ("cooperate", "cooperate"):
+            payoff = {"player1":2, "player2":2}
+        if strategies == ("cooperate", "cheat"):
+            payoff = {"player1":-1, "player2":3}
+        if strategies == ("cheat", "cooperate"):
+            payoff = {"player1":3, "player2":-1}
+        if strategies == ("cheat", "cheat"):
+            payoff = {"player1":0, "player2":0}
+        r[strategies] = payoff
+    return r
